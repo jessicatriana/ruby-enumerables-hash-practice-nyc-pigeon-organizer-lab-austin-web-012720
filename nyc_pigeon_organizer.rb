7ee2f672f00d27d19a6ffda1056987fcb06dcb6f
Jessica Triana <jessica.triana.tx@gmail.com>
2020-01-15 22:42:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+ males = data[:gender][:male]
+ females = data[:gender][:female]
+ pigeons = []
+ pigeons << males
+ pigeons << females
+
+pigeon_names = [] 
+
+first_index = 0 
+
+  while first_index < pigeons.count do 
+  second_index = 0 
+    while second_index < pigeons[first_index].count do
+    pigeon_names << pigeons[first_index][second_index]
+    second_index += 1 
+    end 
+  first_index += 1 
+  end 
+
+result = {}
+
+pigeon_names.each do 
+  if data[:color].include? 
+    result[pigeon_names{index) += data[:color]
+
+result
 end
